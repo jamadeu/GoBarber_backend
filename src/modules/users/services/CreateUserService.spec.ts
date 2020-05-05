6edaa00c58f8f9c30bedf12ba4cf956cf8ee694d
Jean Amadeu <jean.amadeu2@gmail.com>
2020-05-05 14:04:30 -0300

    Fix: Update CreateUser
    Update Service and Tests CreateUser to use hashProvider

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -2,11 +2,16 @@ import AppError from '@shared/errors/AppError';
 
 import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
+import FakeHashProvider from '../Providers/HashProvider/fakes/FakeHashProvider';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUserRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const user = await createUser.execute({
       name: 'Jhon Doe',
@@ -20,7 +25,11 @@ describe('CreateUser', () => {
 
   it('should not be able to create two users with the same email', async () => {
     const fakeUsersRepository = new FakeUserRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     await createUser.execute({
       name: 'Jhon Doe',
