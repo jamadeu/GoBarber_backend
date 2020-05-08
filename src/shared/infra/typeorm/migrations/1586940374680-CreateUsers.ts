e761f0638696aedfebbe46844fea2f645c9a849f
Jean Phillipe <jean.amadeu2@gmail.com>
2020-05-08 09:45:01 -0300

    Create Migration to user_tokens table
    Update migrations CreateAppointment and CreateUsers, update ormconfig

--- src/shared/infra/typeorm/migrations/1586940374680-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1586940374680-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1586940374680 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
