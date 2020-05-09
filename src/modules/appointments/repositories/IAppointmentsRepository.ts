a5fd408704d7b23def7b7e66625dc58cbec4ee27
Jean Phillipe <jean.amadeu2@gmail.com>
2020-05-09 11:06:40 -0300

    Update interface, fakeRepository and repository of appointments
    To use IFindAllInMonthFromProviderDTO

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,8 +1,11 @@
-import Appointment from '../infra/typeorm/entities/Appointment';
-
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
 }
