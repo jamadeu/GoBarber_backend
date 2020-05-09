5bf76afbe348b4e37cc5b8243e9c5054754b089a
Jean Phillipe <jean.amadeu2@gmail.com>
2020-05-09 20:18:27 -0300

    Fix filter data on listMonthAvalability and DayAvailability

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProvidersDayAvailabilityService from '@modules/appointments/services/ListProvidersDayAvailabilityService';
+
+export default class ProviderDayAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { day, month, year } = request.body;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProvidersDayAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(availability);
+  }
+}
