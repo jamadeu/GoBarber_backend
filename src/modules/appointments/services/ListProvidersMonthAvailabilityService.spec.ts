48cc8a61bbe25a6d48f9171315fa1fc7e251eb32
Jean Phillipe <jean.amadeu2@gmail.com>
2020-05-09 13:30:58 -0300

    Finished list availability month

--- src/modules/appointments/services/ListProvidersMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProvidersMonthAvailabilityService.spec.ts
@@ -1,16 +1,11 @@
-// import AppError from '@shared/errors/AppError';
-
-// import FakeUserRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import FakeAppointments from '../repositories/fakes/FakeAppointmentsRepository';
 import ListProvidersMonthAvailabilityService from './ListProvidersMonthAvailabilityService';
 
 let fakeAppointments: FakeAppointments;
-// let fakeUserRepository: FakeUserRepository;
 let listProvidersMonthAvailability: ListProvidersMonthAvailabilityService;
 
 describe('ListProvidersMonthAvailability', () => {
   beforeEach(() => {
-    // fakeUserRepository = new FakeUserRepository();
     fakeAppointments = new FakeAppointments();
     listProvidersMonthAvailability = new ListProvidersMonthAvailabilityService(
       fakeAppointments,
@@ -19,17 +14,51 @@ describe('ListProvidersMonthAvailability', () => {
   it('should be able to list the month availability from provider', async () => {
     await fakeAppointments.create({
       provider_id: 'provider_id',
-      date: new Date(2020, 3, 20, 8, 0, 0),
-    });
-
-    await fakeAppointments.create({
-      provider_id: 'provider_id',
       date: new Date(2020, 4, 20, 8, 0, 0),
     });
 
     await fakeAppointments.create({
       provider_id: 'provider_id',
+      date: new Date(2020, 4, 20, 9, 0, 0),
+    });
+
+    await fakeAppointments.create({
+      provider_id: 'provider_id',
       date: new Date(2020, 4, 20, 10, 0, 0),
+    });
+    await fakeAppointments.create({
+      provider_id: 'provider_id',
+      date: new Date(2020, 4, 20, 11, 0, 0),
+    });
+
+    await fakeAppointments.create({
+      provider_id: 'provider_id',
+      date: new Date(2020, 4, 20, 12, 0, 0),
+    });
+
+    await fakeAppointments.create({
+      provider_id: 'provider_id',
+      date: new Date(2020, 4, 20, 13, 0, 0),
+    });
+
+    await fakeAppointments.create({
+      provider_id: 'provider_id',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    await fakeAppointments.create({
+      provider_id: 'provider_id',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    await fakeAppointments.create({
+      provider_id: 'provider_id',
+      date: new Date(2020, 4, 20, 16, 0, 0),
+    });
+
+    await fakeAppointments.create({
+      provider_id: 'provider_id',
+      date: new Date(2020, 4, 20, 17, 0, 0),
     });
 
     await fakeAppointments.create({
@@ -47,7 +76,7 @@ describe('ListProvidersMonthAvailability', () => {
       expect.arrayContaining([
         { day: 19, available: true },
         { day: 20, available: false },
-        { day: 21, available: false },
+        { day: 21, available: true },
         { day: 22, available: true },
       ]),
     );
--- src/modules/appointments/services/ListProvidersMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProvidersMonthAvailabilityService.ts
@@ -1,8 +1,7 @@
 import { inject, injectable } from 'tsyringe';
-import {} from 'date-fns';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-// import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
   provider_id: string;
@@ -34,9 +33,23 @@ class ListProvidersMonthAvailabilityService {
         year,
       },
     );
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
 
-    return [{ day: 1, available: false }];
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
