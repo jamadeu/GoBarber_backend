62abf427b51bf89b5583cb5b984134a327c5083e
Jean Phillipe <jean.amadeu2@gmail.com>
2020-05-15 08:31:40 -0300

    Simulating AWS config

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailconfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'equipe@gobarber.com.br',
+      name: 'Equipe GoBarber',
+    },
+  },
+} as IMailconfig;
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,11 +1,10 @@
 import { container } from 'tsyringe';
-
+import mailCOnfig from '@config/mail';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
@@ -13,13 +12,13 @@ container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
-
 container.registerSingleton<IMailTemplateProvider>(
   'MailTemplateProvider',
   HandlebarsMailTemplateProvider,
 );
-
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailCOnfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
