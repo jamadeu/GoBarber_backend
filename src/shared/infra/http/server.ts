324bc459f145e9802854ce18fff8df46dbfd6a30
Jean Amadeu <jean.amadeu2@gmail.com>
2020-05-24 16:57:21 -0300

    Chore: change rate limiter
     to not apply  on file router

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -12,10 +12,10 @@ import '@shared/container';
 import rateLimiter from './middlewares/RateLimitier';
 
 const app = express();
-app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
 app.use(routes);
 app.use(errors());
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
